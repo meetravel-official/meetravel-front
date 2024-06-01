04801e0b2fa27de195799058ae82a0039374bf75
iioo3356 <iioo3356@gmail.com>
2024-06-02 03:27:08 +0900

    fix: path alias 에러 수정

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   babel: {
     presets: ["@emotion/babel-preset-css-prop"],
   },
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+          return webpackConfig;
+        },
+      },
+    },
+  ],
 };
