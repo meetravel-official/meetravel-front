a835a7e97e84b08b6f329c3d02f53714ea457535
iioo3356 <iioo3356@gmail.com>
2024-06-17 15:29:22 +0900

    feat: 컬러칩 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  WHITE: "#FFFFFF",
+  GRAY1: "#FFF1F1",
+  GRAY2: "#D0C3C3",
+  GRAY3: "#B1A7A7",
+  GRAY4: "#6F6969",
+  GRAY5: "#393636",
+  PINK1: "#FFD0E1",
+  PINK2: "#FF96AF",
+  PINK3: "#FF6B9C",
+  PINK4: "#B1375C",
+  SITUATION1: "#FF6868",
+  SITUATION2: "#31C26B",
+};
