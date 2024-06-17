74492167f1d4fdb9ee4163081acf9388cd859263
iioo3356 <iioo3356@gmail.com>
2024-06-17 18:31:45 +0900

    fix: 메인 컨테이너에서 h1 태그 삭제

--- src/routes/Main/MainContainer.tsx
+++ src/routes/Main/MainContainer.tsx
@@ -7,7 +7,6 @@ import { pageRoutes } from "..";
 export const MainContainer = () => {
   return (
     <div>
-      <h1>main</h1>
       <UserAvatar size={84} />
       <UserAvatar
         profileUrl="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
