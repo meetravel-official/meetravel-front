ea2349ef1343fd49bd4e935ca8ad6f23f3452d71
iioo3356 <iioo3356@gmail.com>
2024-06-18 13:52:49 +0900

    feat: 프리텐다드 가변 다이나믹 서브셋 추가

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -3,9 +3,15 @@ import { css } from "@emotion/react";
 import { COLORS } from "./color";
 
 export const cssGlobalStyle = css`
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.9/variable/pretendardvariable-dynamic-subset.min.css");
+
   body {
     background: ${COLORS.GRAY2};
     margin: 0;
     padding: 0;
+    font-family: "Pretendard Variable", Pretendard, -apple-system,
+      BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
+      "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
+      "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 `;
