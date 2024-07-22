faca089772b82aed07fb466cc4947b413009a215
iioo3356 <iioo3356@gmail.com>
2024-07-22 17:45:57 +0900

    feat: Image javadoc 추가

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -21,6 +21,17 @@ interface ImageProps extends ImageStyle {
   draggable?: boolean;
 }
 
+/**
+ * Image 이미지 컴포넌트(로딩, 에러 처리)
+ * 이미지 로딩 시 로딩 화면 표시, 이미지 로딩 실패 시 실패 이미지 표시 처리함
+ * lazy loading 적용
+ * @param src 이미지 주소(필수)
+ * @param alt 이미지 대체 텍스트(필수)
+ * @param width 이미지 너비(default: 100%)
+ * @param height 이미지 높이(default: auto)
+ * @param objectFit 이미지 크기 조절(default: cover)
+ * @param draggable 드래그 가능 여부(default: false)
+ */
 export const Image = ({
   src,
   alt,
