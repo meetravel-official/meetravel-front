103e1bff87c1d8377bbab0d9dc9525665df1f50e
iioo3356 <iioo3356@gmail.com>
2024-07-22 17:40:44 +0900

    feat: Image 컴포넌트 추가

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+import NoImageSvg from "@/assets/icons/no-image.svg";
+
+import {
+  cssImageContainerStyle,
+  cssImageStyle,
+  cssLoadingStyle,
+  cssNoImageStyle,
+} from "./Image.styles";
+
+export interface ImageStyle {
+  width?: string;
+  height?: string;
+  objectFit?: React.CSSProperties["objectFit"];
+}
+
+interface ImageProps extends ImageStyle {
+  src: string;
+  alt: string;
+  draggable?: boolean;
+  loading?: HTMLImageElement["loading"];
+}
+
+export const Image = ({
+  src,
+  alt,
+  width,
+  height,
+  objectFit,
+  draggable = false,
+}: ImageProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const handleOnLoad = () => {
+    setIsLoading(false);
+  };
+
+  return isError ? (
+    <div css={cssNoImageStyle({ width, height })}>
+      <img src={NoImageSvg} alt={alt} draggable={false} />
+    </div>
+  ) : (
+    <div css={cssImageContainerStyle({ width, height })}>
+      <img
+        className="image"
+        css={cssImageStyle({ width, height, objectFit })}
+        src={src}
+        alt={alt}
+        onError={() => {
+          setIsError(true);
+        }}
+        onLoad={handleOnLoad}
+        draggable={draggable}
+        loading="lazy"
+      />
+      {isLoading && <div css={cssLoadingStyle} />}
+    </div>
+  );
+};
