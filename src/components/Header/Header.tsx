0239afb21f4b66766c0a2b03c73b481d0df8e456
iioo3356 <iioo3356@gmail.com>
2024-08-11 17:37:34 +0900

    fix: header에 자바독 추가 및 가운데 영역에 react node 넣을 수 있도록 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,19 +8,31 @@ import { cssHeaderCenterStyle, cssHeaderStyle } from "./Header.styles";
 
 interface HeaderProps {
   prefix?: ReactNode;
+  titleContent?: ReactNode;
   suffix?: ReactNode;
 }
 
-export const Header = ({ prefix, suffix }: HeaderProps) => {
+/**
+ * Header
+ * @description 레이아웃이나 페이지 상단에 직접 이용 가능
+ * @param prefix - 왼쪽 컨트롤 컴포넌트 지정
+ * @param titleContent - zustand로 지정한 타이틀 대신 reactNode 지정 가능
+ * @param suffix - 오른쪽 컨트롤 컴포넌트 지정
+ */
+export const Header = ({ prefix, titleContent, suffix }: HeaderProps) => {
   const { title } = useHeaderState();
 
   return (
     <div css={cssHeaderStyle}>
       {prefix && <div>{prefix}</div>}
       <div css={cssHeaderCenterStyle}>
-        <Typography color={COLORS.GRAY3} size="20" weight="bold">
-          {title}
-        </Typography>
+        {titleContent ? (
+          titleContent
+        ) : (
+          <Typography color={COLORS.GRAY3} size="20" weight="bold">
+            {title}
+          </Typography>
+        )}
       </div>
       {suffix && <div>{suffix}</div>}
     </div>
