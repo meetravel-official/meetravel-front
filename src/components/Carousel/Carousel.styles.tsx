2fd52b8fad4770c94995f5a9db270504550a7618
iioo3356 <iioo3356@gmail.com>
2024-08-27 23:40:52 +0900

    feat: 캐러셀 추가

---
+++ src/components/Carousel/Carousel.styles.tsx
@@ -0,0 +1,59 @@
+import { css } from "@emotion/react";
+
+import { WIDTH } from "@/styles/breakpoint";
+import { COLORS } from "@/styles/color";
+
+export const cssCarouselStyle = css`
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const cssCarouselInnerStyle = (index: number, count: number) => css`
+  width: max-content;
+
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+
+  transition: all 0.5s;
+
+  transform: translateX(
+    calc(-100% / ${count + 2} - 100% / ${count + 2} * ${index} + 30px)
+  );
+`;
+
+export const cssCarouselItemStyle = css`
+  width: calc(100vw - 32px - 60px); //layout padding - 캐러셀 양 옆 살짝 보이게
+  max-width: calc(${WIDTH.SM} - 32px - 60px); // width가 한없이 커지는 거 방지
+  height: auto;
+  aspect-ratio: 300 / 176;
+  border-radius: 8px;
+  border: 1px solid ${COLORS.GRAY2};
+  overflow: hidden;
+  img {
+    object-fit: cover;
+  }
+`;
+
+export const cssCarouselDotBoxStyle = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 4px;
+
+  position: absolute;
+  bottom: 8px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 2;
+`;
+
+export const cssCarouselDotStyle = (isActive: boolean) => css`
+  width: 8px;
+  height: 8px;
+  padding: 0;
+  background: ${isActive ? COLORS.GRAY1 : "transparent"};
+  border: 1px solid ${COLORS.GRAY1};
+  border-radius: 50%;
+`;
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from "react";
+
+import {
+  cssCarouselDotBoxStyle,
+  cssCarouselDotStyle,
+  cssCarouselInnerStyle,
+  cssCarouselItemStyle,
+  cssCarouselStyle,
+} from "./Carousel.styles";
+
+interface CarouselProps {
+  children: ReactNode[];
+}
+export const Carousel = ({ children }: CarouselProps) => {
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+
+  const infiniteArray = (arr: ReactNode[]) => {
+    const startItem = arr[0];
+    const endItem = arr[arr.length - 1];
+    const modifiedArray = [endItem, ...arr, startItem];
+    return modifiedArray;
+  };
+
+  const handleOnClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div css={cssCarouselStyle}>
+      <div css={cssCarouselInnerStyle(currentIndex, children.length)}>
+        {infiniteArray(children).map((child, index) => (
+          <div css={cssCarouselItemStyle} key={index}>
+            {child}
+          </div>
+        ))}
+      </div>
+      <div css={cssCarouselDotBoxStyle}>
+        {React.Children.map(children, (_child, index) => (
+          <button
+            key={index}
+            css={cssCarouselDotStyle(index === currentIndex)}
+            onClick={() => handleOnClick(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
