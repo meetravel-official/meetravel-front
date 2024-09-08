b60802e8b17c83f73706786893a1fc3edfd5cf92
iioo3356 <iioo3356@gmail.com>
2024-09-08 21:44:58 +0900

    feat: 관광공사 api 추가

--- src/api/hosts/index.ts
+++ src/api/hosts/index.ts
@@ -1,2 +1,4 @@
-export const HOST = process.env.NEXT_PUBLIC_HOST;
-export const HOST_API: string = process.env.NEXT_PUBLIC_HOST_API || "";
+export const HOST_URL = process.env.REACT_APP_HOST_URL || "";
+export const HOST_API_URL: string = process.env.REACT_APP_HOST_API_URL || "";
+export const VISIT_KOREA_API_URL =
+  process.env.REACT_APP_KOREA_VISIT_API_URL || "";
--- src/api/routes/apiRoutes.ts
+++ src/api/routes/apiRoutes.ts
@@ -1,5 +1,11 @@
-import { HOST_API } from "../hosts";
+import { HOST_API_URL, VISIT_KOREA_API_URL } from "../hosts";
+
+const visitKoreaApiBaseRoute = {
+  koreanVisitService: `${VISIT_KOREA_API_URL}/KorService1`, // 한국관광공사_국문 관광정보 서비스
+};
 
 export const apiRoute = {
-  sample: `${HOST_API}/sample`,
+  sample: `${HOST_API_URL}/sample`,
+  areaCode: `${visitKoreaApiBaseRoute.koreanVisitService}/areaCode1`, // 지역코드 조회
+  areaBasedList: `${visitKoreaApiBaseRoute.koreanVisitService}/areaBasedList1`, // 지역기반 관광정보 조회
 };
