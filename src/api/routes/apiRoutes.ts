6c45b9f8b6d536146d4b133f87df7d6e321afc5d
iioo3356 <iioo3356@gmail.com>
2024-09-08 21:45:44 +0900

    fix: 샘플 api 삭제

--- src/api/routes/apiRoutes.ts
+++ src/api/routes/apiRoutes.ts
@@ -1,11 +1,10 @@
-import { HOST_API_URL, VISIT_KOREA_API_URL } from "../hosts";
+import { VISIT_KOREA_API_URL } from "../hosts";
 
 const visitKoreaApiBaseRoute = {
   koreanVisitService: `${VISIT_KOREA_API_URL}/KorService1`, // 한국관광공사_국문 관광정보 서비스
 };
 
 export const apiRoute = {
-  sample: `${HOST_API_URL}/sample`,
   areaCode: `${visitKoreaApiBaseRoute.koreanVisitService}/areaCode1`, // 지역코드 조회
   areaBasedList: `${visitKoreaApiBaseRoute.koreanVisitService}/areaBasedList1`, // 지역기반 관광정보 조회
 };
