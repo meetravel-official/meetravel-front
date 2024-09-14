8d90b12d1ee9fe1203895c3576a1c181167b65c7
suyun <suyunup@kookmin.ac.kr>
2024-09-14 22:17:04 +0900

    fix: BarStep 수정

--- src/components/BarStep/BarStep.tsx
+++ src/components/BarStep/BarStep.tsx
@@ -1,14 +1,20 @@
+import { SerializedStyles } from "@emotion/react";
+
 import { Bar, Step } from "@/components";
 import { StepProps } from "@/components/Step/StepInterface";
 import { cssAlignVerticalStyle } from "@/styles/align";
 
-export const BarStep = ({ step, stepList }: StepProps) => {
+export const BarStep = ({
+  step,
+  stepList,
+  contentDetailStyle,
+}: StepProps & { contentDetailStyle?: SerializedStyles }) => {
   return (
     <Step step={step} stepList={stepList}>
       <div css={cssAlignVerticalStyle({ gap: 16, alignItems: "flex-start" })}>
         <Step.Stepper disabled="all" />
         <Bar />
-        <Step.Content />
+        <Step.Content detailStyle={contentDetailStyle} />
       </div>
     </Step>
   );
