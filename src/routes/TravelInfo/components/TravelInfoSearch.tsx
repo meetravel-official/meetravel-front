0650d0c2af89d7fe691deb1727dc982b891b5ad6
iioo3356 <iioo3356@gmail.com>
2024-09-14 14:34:25 +0900

    fix: 여행 정보 탭에서 셀렉트가 아닌 모달로 검색값 선택

--- src/routes/TravelInfo/components/TravelInfoSearch.tsx
+++ src/routes/TravelInfo/components/TravelInfoSearch.tsx
@@ -1,22 +1,36 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useTravelInfo } from "states/useTravelInfo";
+import { getShortAreaName } from "utils/area-utils";
 
 import { useGetAreaCode } from "@/api/hooks/visitKorea";
 import { IAriaCode } from "@/api/interfaces/visitKorea";
-import Select from "@/components/Select/Select";
+import { ReactComponent as SelectArrow } from "@/assets/icons/select-arrow.svg";
+import { Button, Typography } from "@/components";
+import Modal from "@/components/Modal/Modal";
 import { cssAlignHorizontalStyle } from "@/styles/align";
+import { COLORS } from "@/styles/color";
+
+import {
+  cssAreaCodeSelectListStyle,
+  cssContentTypeSelectListStyle,
+  cssSelectBtnStyle,
+} from "../styles/TravelInfoSearch.styles";
 
 export const TravelInfoSearch = () => {
   const { searchValue, setSearchValue } = useTravelInfo();
 
   const { data: areaCodeData } = useGetAreaCode();
 
+  const [isOpenAreaSelectModal, setIsOpenAreaSelectModal] = useState(false);
+  const [isOpenContentTypeSelectModal, setIsOpenContentTypeSelectModal] =
+    useState(false);
+
   const areaCodeList = useMemo(() => {
     if (areaCodeData?.data.response?.body?.items?.item) {
       return areaCodeData.data.response.body.items.item.map(
         (item: IAriaCode) => ({
-          key: item.code,
-          value: item.name,
+          code: item.code,
+          label: getShortAreaName(item.name),
         })
       );
     }
@@ -25,45 +39,44 @@ export const TravelInfoSearch = () => {
 
   const contentTypeIdList = useMemo(() => {
     return [
-      { key: "12", value: "관광지" },
-      { key: "14", value: "문화시설" },
-      { key: "15", value: "축제공연행사" },
-      { key: "28", value: "레포츠" },
-      { key: "32", value: "숙박" },
-      { key: "38", value: "쇼핑" },
-      { key: "39", value: "음식점" },
+      { contentTypeId: "12", label: "관광지" },
+      { contentTypeId: "14", label: "문화시설" },
+      { contentTypeId: "15", label: "축제공연행사" },
+      { contentTypeId: "28", label: "레포츠" },
+      { contentTypeId: "32", label: "숙박" },
+      { contentTypeId: "38", label: "쇼핑" },
+      { contentTypeId: "39", label: "음식점" },
     ];
   }, []);
 
-  const handleOnChangeAreaCode = useCallback(
-    (selectedValue: string) => {
-      const selectedItem = areaCodeList.find(
-        (item: { key: string; value: string }) => item.value === selectedValue
-      );
-      if (selectedItem)
-        setSearchValue({
-          ...searchValue,
-          areaCodeLabel: selectedItem.value,
-          areaCode: selectedItem.key,
-        });
+  const handleOnSelectAreaCode = useCallback(
+    (selectedAreaCode: { code: string; label: string }) => {
+      setIsOpenAreaSelectModal(false);
+      setSearchValue({
+        ...searchValue,
+        areaCode: selectedAreaCode.code,
+        areaCodeLabel: selectedAreaCode.label,
+      });
     },
-    [areaCodeList, searchValue, setSearchValue]
+    [searchValue, setSearchValue]
   );
 
-  const handleOnChangeContentTypeId = useCallback(
-    (selectedValue: string) => {
-      const selectedItem = contentTypeIdList.find(
-        (item: { key: string; value: string }) => item.value === selectedValue
-      );
-      if (selectedItem)
-        setSearchValue({
-          ...searchValue,
-          contentTypeId: selectedItem.key,
-          contentTypeIdLabel: selectedItem.value,
-        });
+  const handleOnSelectContentType = useCallback(
+    (selectedContentType: { contentTypeId: string; label: string }) => {
+      setIsOpenContentTypeSelectModal(false);
+      setSearchValue({
+        ...searchValue,
+        contentTypeId: selectedContentType.contentTypeId,
+        contentTypeIdLabel: selectedContentType.label,
+      });
     },
-    [contentTypeIdList, searchValue, setSearchValue]
+    [searchValue, setSearchValue]
   );
+
+  const handleOnClickSelectBtn = useCallback((type: "area" | "contentType") => {
+    if (type === "area") setIsOpenAreaSelectModal(true);
+    else setIsOpenContentTypeSelectModal(true);
+  }, []);
 
   return (
     <div
@@ -71,24 +84,80 @@ export const TravelInfoSearch = () => {
         gap: 4,
       })}
     >
-      <Select
-        width="106px"
-        placeholder="지역 선택"
-        value={searchValue.areaCodeLabel}
-        selectOptions={areaCodeList}
-        onChange={(selectedValue) =>
-          handleOnChangeAreaCode(selectedValue as string)
-        }
-      />
-      <Select
-        width="138px"
-        placeholder="여행 타입 선택"
-        selectOptions={contentTypeIdList}
-        value={searchValue.contentTypeIdLabel}
-        onChange={(selectedValue) => {
-          handleOnChangeContentTypeId(selectedValue as string);
+      <button
+        css={cssSelectBtnStyle(110)}
+        onClick={() => {
+          handleOnClickSelectBtn("area");
         }}
-      />
+      >
+        <Typography color={COLORS.GRAY3} weight="bold" size="16">
+          {searchValue.areaCodeLabel || "지역 선택"}
+        </Typography>
+        <SelectArrow
+          width={12}
+          height={12}
+          stroke={COLORS.GRAY3}
+          strokeWidth={2}
+        />
+      </button>
+      <button
+        css={cssSelectBtnStyle(138)}
+        onClick={() => {
+          handleOnClickSelectBtn("contentType");
+        }}
+      >
+        <Typography color={COLORS.GRAY3} weight="bold" size="16">
+          {searchValue.contentTypeIdLabel || "여행 타입 선택"}
+        </Typography>
+        <SelectArrow
+          width={12}
+          height={12}
+          stroke={COLORS.GRAY3}
+          strokeWidth={2}
+        />
+      </button>
+      <Modal
+        isOpen={isOpenAreaSelectModal}
+        onClose={() => {
+          setIsOpenAreaSelectModal(false);
+        }}
+        modalType="simple"
+        title="지역 선택"
+      >
+        <div css={cssAreaCodeSelectListStyle}>
+          {areaCodeList.map((item) => (
+            <Button
+              key={item.code}
+              onClick={() => handleOnSelectAreaCode(item)}
+            >
+              <Typography color={COLORS.GRAY5} weight="bold">
+                {item.label}
+              </Typography>
+            </Button>
+          ))}
+        </div>
+      </Modal>
+      <Modal
+        isOpen={isOpenContentTypeSelectModal}
+        onClose={() => {
+          setIsOpenContentTypeSelectModal(false);
+        }}
+        modalType="simple"
+        title="여행 타입 선택"
+      >
+        <div css={cssContentTypeSelectListStyle}>
+          {contentTypeIdList.map((item) => (
+            <Button
+              key={item.contentTypeId}
+              onClick={() => handleOnSelectContentType(item)}
+            >
+              <Typography color={COLORS.GRAY5} weight="bold">
+                {item.label}
+              </Typography>
+            </Button>
+          ))}
+        </div>
+      </Modal>
     </div>
   );
 };
