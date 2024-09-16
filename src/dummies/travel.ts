ed2dec89331490f707898edbb7546553c4c07a77
iioo3356 <iioo3356@gmail.com>
2024-09-17 02:06:40 +0900

    chore: 더미 여행 정보 추가

---
+++ src/dummies/travel.ts
@@ -0,0 +1,25 @@
+export const dummyTravelInfo = {
+  addr1: "경상북도 안동시 하회남촌길 69-5",
+  addr2: "",
+  areacode: "35",
+  booktour: "",
+  cat1: "B02",
+  cat2: "B0201",
+  cat3: "B02011600",
+  contentid: "2465071",
+  contenttypeid: "32",
+  cpyrhtDivCd: "Type3",
+  createdtime: "20161220190700",
+  firstimage:
+    "http://tong.visitkorea.or.kr/cms/resource/00/2626200_image2_1.jpg",
+  firstimage2:
+    "http://tong.visitkorea.or.kr/cms/resource/00/2626200_image3_1.jpg",
+  mapx: "128.5175868107",
+  mapy: "36.5376537450",
+  mlevel: "6",
+  modifiedtime: "20230413082505",
+  sigungucode: "11",
+  tel: "054-855-8552",
+  title: "가경재 [한국관광 품질인증/Korea Quality]",
+  zipcode: "36760",
+};
