80e9201c529fbe3d2d0a8287db17b333e866bc6c
iioo3356 <iioo3356@gmail.com>
2024-09-22 18:54:44 +0900

    feat: 홈페이지 - 실시간 여행 매칭 섹션 UI 추가

--- src/routes/Home/HomeContainer.tsx
+++ src/routes/Home/HomeContainer.tsx
@@ -1,10 +1,12 @@
 import { css } from "@emotion/react";
-import { useState } from "react";
+import { dummyChatData } from "dummies/chat";
+import { useCallback, useMemo, useState } from "react";
 
 import { ReactComponent as Bell } from "@/assets/icons/bell.svg";
-import { ReactComponent as Pen } from "@/assets/icons/pen.svg";
 import { ReactComponent as Search } from "@/assets/icons/search.svg";
 import { Button, Typography } from "@/components";
+import { ArrowButton } from "@/components/ArrowButton/ArrowButton";
+import ChatItem, { ChatStatus } from "@/components/Chat/ChatItem";
 import Input from "@/components/Input/Input";
 import { cssAlignHorizontalStyle, cssAlignVerticalStyle } from "@/styles/align";
 import { COLORS } from "@/styles/color";
@@ -13,6 +15,39 @@ import { BannerCarousel } from "./components/BannerCarousel";
 import { cssHomeContainerStyle } from "./HomeContainer.styles";
 export const HomeContainer = () => {
   const [value, setValue] = useState<string>("");
+
+  const pageSize = 3;
+  const [page, setPage] = useState<number>(0);
+
+  const chatDataList = useMemo(() => {
+    const arr = [];
+    for (let i = 0; i < 9; i++) {
+      arr.push({
+        ...dummyChatData,
+        isActive: false, // TODO: 현재 컴포넌트의 색상 변경 로직 떄문에 일단 해당 상태로 넣어둠
+        status: ChatStatus.REVIEW, // TODO: 현재 컴포넌트의 색상 변경 로직 떄문에 일단 해당 상태로 넣어둠
+        title: "채팅방" + i.toString(),
+        link: "/chat/" + i.toString(),
+      });
+    }
+    return arr;
+  }, []);
+
+  const chatDataListByPage = useMemo(() => {
+    return chatDataList.slice(page * 3, page * 3 + 3);
+  }, [chatDataList, page]);
+
+  const maxPage = useMemo(() => {
+    return Math.ceil(chatDataList.length / pageSize);
+  }, [chatDataList]);
+
+  const handleOnPrevPage = useCallback(() => {
+    if (page > 0) setPage(page - 1);
+  }, [page]);
+
+  const handleOnNextPage = useCallback(() => {
+    if (page < maxPage - 1) setPage(page + 1);
+  }, [page, maxPage]);
 
   return (
     <div css={cssHomeContainerStyle}>
@@ -32,34 +67,51 @@ export const HomeContainer = () => {
         />
         <div css={cssAlignVerticalStyle({ gap: 20 })}>
           <BannerCarousel />
-          <div css={cssAlignVerticalStyle({ gap: 8 })}>
-            <Button
-              icon={<Bell />}
-              height="large"
-              align="start"
-              link
-              linkColor={COLORS.GRAY2}
-            >
-              <Typography color={COLORS.GRAY4} weight="bold" size={16}>
-                알림 내역
-              </Typography>
-            </Button>
-            <Button
-              icon={<Pen />}
-              height="large"
-              align="start"
-              link
-              linkColor={COLORS.GRAY2}
-            >
-              <div css={cssAlignHorizontalStyle({ gap: 4 })}>
-                <Typography color={COLORS.GRAY4} weight="bold" size={16}>
-                  작성할 수 있는 후기
-                </Typography>
-                <Typography color={COLORS.PINK3} weight="bold" size={16}>
-                  +12
-                </Typography>
+          <Button
+            icon={<Bell />}
+            height="large"
+            align="start"
+            link
+            linkColor={COLORS.GRAY2}
+          >
+            <Typography color={COLORS.GRAY4} weight="bold" size={16}>
+              알림 내역
+            </Typography>
+          </Button>
+
+          <div
+            css={cssAlignVerticalStyle({ gap: 8, alignItems: "flex-start" })}
+          >
+            <Typography color={COLORS.GRAY3} weight="bold" size={16}>
+              실시간 여행 매칭
+            </Typography>
+            <div css={cssAlignVerticalStyle({ gap: 16 })}>
+              <div css={cssAlignVerticalStyle({ gap: 8 })}>
+                {chatDataListByPage.map((chatData) => (
+                  <ChatItem key={chatData.link} chatData={chatData} />
+                ))}
               </div>
-            </Button>
+              <div css={cssAlignHorizontalStyle({ gap: 38 })}>
+                <ArrowButton
+                  direction="left"
+                  disabled={page === 0}
+                  onClick={handleOnPrevPage}
+                />
+                <div>
+                  <Typography color={COLORS.GRAY4} size={20} weight={700}>
+                    {page + 1}
+                  </Typography>{" "}
+                  <Typography color={COLORS.GRAY3} size={20} weight={700}>
+                    / {maxPage}
+                  </Typography>
+                </div>
+                <ArrowButton
+                  direction="right"
+                  disabled={page === maxPage - 1}
+                  onClick={handleOnNextPage}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
