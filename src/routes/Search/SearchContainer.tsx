c76e197fe842b2f1aa0e448f6b1fdfdddeadd3c7
iioo3356 <iioo3356@gmail.com>
2024-09-24 16:59:55 +0900

    feat: 검색 화면 > 채팅방 클릭 시 들어갈지 모달 출력

--- src/routes/Search/SearchContainer.tsx
+++ src/routes/Search/SearchContainer.tsx
@@ -1,11 +1,16 @@
 import { dummyChatData } from "dummies/chat";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { checkUser } from "utils/check-user";
 
-import ChatItem, { ChatStatus } from "@/components/Chat/ChatItem";
+import ChatItem, { ChatStatus, IChatData } from "@/components/Chat/ChatItem";
+import { EnterChatRoomModal } from "@/components/EnterChatRoomModal/EnterChatRoomModal";
 import { cssAlignVerticalStyle } from "@/styles/align";
+import { cssDefaultBtnStyle } from "@/styles/button";
 
 export const SearchContainer = checkUser(() => {
+  const [selectedChatData, setSelectedChatData] = useState<IChatData>();
+  const [isOpenEnterChatModal, setIsOpenEnterChatModal] = useState(false);
+
   const chatDataList = useMemo(() => {
     const arr = [];
     for (let i = 0; i < 10; i++) {
@@ -20,11 +25,31 @@ export const SearchContainer = checkUser(() => {
     return arr;
   }, []);
 
+  const handleOnClickChat = (chatData: IChatData) => {
+    setSelectedChatData(chatData);
+    setIsOpenEnterChatModal(true);
+  };
+
+  const handleOnCloseEnterChatModal = () => {
+    setIsOpenEnterChatModal(false);
+  };
+
   return (
     <div css={cssAlignVerticalStyle({ gap: 8 })}>
       {chatDataList.map((chatData) => (
-        <ChatItem key={chatData.link} chatData={chatData} />
-      ))}{" "}
+        <button
+          key={chatData.link}
+          css={cssDefaultBtnStyle({ width: "100%" })}
+          onClick={() => handleOnClickChat(chatData)}
+        >
+          <ChatItem chatData={chatData} />
+        </button>
+      ))}
+      <EnterChatRoomModal
+        isOpen={isOpenEnterChatModal}
+        onClose={handleOnCloseEnterChatModal}
+        chatData={selectedChatData}
+      />
     </div>
   );
 });
