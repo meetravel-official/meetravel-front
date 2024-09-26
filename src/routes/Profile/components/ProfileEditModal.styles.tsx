cceb62dd36fe7361b31ebd60f3c51ae723bf5c0d
iioo3356 <iioo3356@gmail.com>
2024-09-26 16:03:43 +0900

    fix: 마이페이지 라디오 버튼 active 스타일 변경

--- src/routes/Profile/components/ProfileEditModal.styles.tsx
+++ src/routes/Profile/components/ProfileEditModal.styles.tsx
@@ -11,4 +11,6 @@ export const cssRadioButtonStyle = (isActive: boolean) => css`
   width: 100%;
   height: 48px;
   background-color: ${isActive ? COLORS.GRAY1 : COLORS.GRAY2};
+  outline: ${isActive ? `1px solid ${COLORS.PINK3}` : "none"} !important;
+  color: ${isActive ? COLORS.PINK3 : COLORS.GRAY5};
 `;
