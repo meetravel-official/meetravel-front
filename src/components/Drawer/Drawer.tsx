f282171f0c26636ef35b0910edebe779d2fe477d
suyun <suyunup@kookmin.ac.kr>
2024-09-28 21:52:05 +0900

    feat: 드로어 컴포넌트 추가

---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,59 @@
+import { css, SerializedStyles } from "@emotion/react";
+
+import { WIDTH } from "@/styles/breakpoint";
+
+import Modal, { ModalType } from "../Modal/Modal";
+
+export interface DrawerProps {
+  isOpen: boolean;
+  onClose: () => void;
+  modalDetailStyle?: SerializedStyles;
+  title?: string | React.ReactNode;
+  closableIcon?: boolean;
+  modalType?: ModalType;
+  footer?: React.ReactNode;
+  children?: React.ReactNode;
+  zIndex?: number;
+}
+
+const Drawer = ({ isOpen, onClose, children }: DrawerProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      modalType={"simple"}
+      closableIcon={false}
+      isAutoClose={true}
+      modalDetailStyle={css`
+        all: unset;
+        box-sizing: border-box;
+        position: fixed;
+        bottom: 0;
+        z-index: 100;
+        width: 100%;
+
+        height: 150px; // TODO: feed 추가되면 늘리기 224px
+        left: 50%;
+        max-width: ${WIDTH.SM};
+        border-radius: 8px 8px 0 0;
+        padding: 20px 8px 0 8px;
+
+        background-color: white;
+        animation: fadeInUp 0.3s ease-in-out forwards;
+        @keyframes fadeInUp {
+          from {
+            transform: translate(-50%, -50%) translateY(100%);
+            opacity: 0;
+          }
+          to {
+            transform: translate(-50%, 0%) translateY(0);
+            opacity: 1;
+          }
+        }
+      `}
+    >
+      {children}
+    </Modal>
+  );
+};
+export default Drawer;
