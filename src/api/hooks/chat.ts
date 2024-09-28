b878d54ea5d5741dca19a898c1b8eb58869b92ed
suyun <suyunup@kookmin.ac.kr>
2024-09-28 21:51:14 +0900

    feat: 채팅방 입장 유저 리스트 조회 추가

--- src/api/hooks/chat.ts
+++ src/api/hooks/chat.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
-import { IMatchingData } from "../interfaces/chat";
+import { IChatUserData, IMatchingData } from "../interfaces/chat";
 import { api } from "../request";
 import { chatApiRoute } from "../routes/apiRoutes";
 
@@ -13,5 +13,12 @@ export const useGetChatRooms = () => {
   return useQuery<IChatRoomsResponse, AxiosError>({
     queryKey: ["useGetChatRooms"],
     queryFn: () => api.get(chatApiRoute.chatRooms),
+  });
+};
+
+export const useGetChatUsers = (chatRoomId: string) => {
+  return useQuery<IChatUserData, AxiosError>({
+    queryKey: ["useGetChatUsers"],
+    queryFn: () => api.get(`${chatApiRoute.chatRooms}/${chatRoomId}/users`),
   });
 };
