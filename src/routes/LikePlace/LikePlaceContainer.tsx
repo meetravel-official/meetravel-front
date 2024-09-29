0e7934e71480987e21e51e245ae703014578b613
iioo3356 <iioo3356@gmail.com>
2024-09-30 03:01:10 +0900

    feat: 좋아요한 여행 정보 탭 내용 추가

--- src/routes/LikePlace/LikePlaceContainer.tsx
+++ src/routes/LikePlace/LikePlaceContainer.tsx
@@ -1,24 +1,66 @@
 import { css } from "@emotion/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { TravelInfoItem } from "routes/TravelInfo/components/TravelInfoItem";
 import { useHeaderState } from "states/useHeader";
 
+import { IAreaBasedList } from "@/api/interfaces/visitKorea";
 import NotFound from "@/components/NotFound/NotFound";
+import { TravelInfoDetailModal } from "@/components/TravelInfoDetailModal/TravelInfoDetailModal";
+import { cssAlignVerticalStyle } from "@/styles/align";
 
 export const LikePlaceContainer = () => {
   const { setTitle } = useHeaderState();
 
+  const [isOpenTravelInfoDetailModal, setIsOpenTravelInfoDetailModal] =
+    useState(false);
+  const [selectedContent, setSelectedContent] = useState<IAreaBasedList>();
+
+  const likePlaceList: string[] =
+    typeof localStorage.getItem("likePlaceList") === "string"
+      ? JSON.parse(localStorage.getItem("likePlaceList") as string)
+      : [];
+
+  const travelInfo = (contentid: string) => {
+    if (
+      localStorage.getItem(contentid) &&
+      typeof localStorage.getItem(contentid) === "string"
+    ) {
+      return JSON.parse(localStorage.getItem(contentid) as string);
+    }
+  };
+
+  const handleOnClickTravelInfoItem = (travelInfo: IAreaBasedList) => {
+    setSelectedContent(travelInfo);
+    setIsOpenTravelInfoDetailModal(true);
+  };
+
   useEffect(() => {
-    setTitle("좋아요한 여행 장소");
+    setTitle("좋아요한 여행 정보");
   }, [setTitle]);
 
   return (
-    <div>
-      <NotFound
-        mainText="아직 좋아요한 여행 장소가 없습니다"
-        subText="여행 정보 탭에서 마음에 드는 장소에 하트를 눌러 주세요!"
-        detailStyle={css`
-          margin-top: 100px;
-        `}
+    <div css={cssAlignVerticalStyle({ gap: 8 })}>
+      {likePlaceList.length > 0 ? (
+        likePlaceList.map((contentid) => (
+          <TravelInfoItem
+            key={contentid}
+            travelInfo={travelInfo(contentid)}
+            onClickItem={handleOnClickTravelInfoItem}
+          />
+        ))
+      ) : (
+        <NotFound
+          mainText="아직 좋아요한 여행 정보가 없습니다"
+          subText="여행 정보 탭에서 마음에 드는 정보에 하트를 눌러 주세요!"
+          detailStyle={css`
+            margin-top: 100px;
+          `}
+        />
+      )}
+      <TravelInfoDetailModal
+        isOpen={isOpenTravelInfoDetailModal}
+        onClose={() => setIsOpenTravelInfoDetailModal(false)}
+        travelInfo={selectedContent}
       />
     </div>
   );
