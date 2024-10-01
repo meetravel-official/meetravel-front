519aecc8a65e9e22c3d9a71372a227fdbaf865fc
iioo3356 <iioo3356@gmail.com>
2024-10-01 17:03:23 +0900

    fix: 홈화면 최소 높이 지정 해제

--- src/routes/Home/HomeContainer.styles.tsx
+++ src/routes/Home/HomeContainer.styles.tsx
@@ -3,6 +3,5 @@ import { css } from "@emotion/react";
 import { cssAlignVerticalStyle } from "@/styles/align";
 
 export const cssHomeContainerStyle = css`
-  min-height: calc(var(--vh, 1vh) * 100 - 64px - 32px);
   ${cssAlignVerticalStyle({ gap: 16, justifyContent: "space-between" })}
 `;
