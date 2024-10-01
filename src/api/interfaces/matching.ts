f54467a3e9a339160fe9c94d3e8ffae95e26506a
suyun <suyunup@kookmin.ac.kr>
2024-10-01 17:25:20 +0900

    feat: 매칭신청서 인터페이스 추가

---
+++ src/api/interfaces/matching.ts
@@ -0,0 +1,36 @@
+import { IArea, IDuration, IPerson } from "./chat";
+
+export interface IMatchingArea {
+  code: string;
+  name: string;
+}
+export interface IMatchingDetailArea {
+  detailCode: string;
+  detailName: string;
+}
+
+export interface IMatchingForm {
+  duration: string;
+  startDate: string;
+  endDate: string;
+  groupSize: string;
+  genderRatio: string;
+  cost: string;
+  area: IMatchingArea;
+  detailArea: IMatchingDetailArea;
+  travelKeywordList: string[];
+}
+
+export interface IUpdateMatchingForm {
+  matchingFormId: number;
+  duration: string;
+  startDate: string;
+  endDate: string;
+  groupSize: string;
+  genderRatio: string;
+  cost: string;
+  area: IMatchingArea;
+  detailArea: IMatchingDetailArea;
+  travelKeywordToDelete: number;
+  travelKeywordToAdd: string[];
+}
