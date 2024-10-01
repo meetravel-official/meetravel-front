f1830b16c3fe0e2d7fdfccf5b8576509c48c67bd
suyun <suyunup@kookmin.ac.kr>
2024-10-01 21:02:42 +0900

    feat: 매칭 api 추가

---
+++ src/api/hooks/matching.ts
@@ -0,0 +1,45 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+
+import { IMatchingForm, IUpdateMatchingForm } from "../interfaces/matching";
+import { api } from "../request";
+import { matchingApiRoute } from "../routes/apiRoutes";
+
+export const usePostMatchingForm = () => {
+  return useMutation<AxiosResponse, AxiosError, IMatchingForm>({
+    mutationFn: (params: IMatchingForm) => {
+      return api.post(matchingApiRoute.matchingForm, params);
+    },
+  });
+};
+
+interface IMatchingResult {
+  matchingFormId: string | null;
+}
+
+export const useGetMatchingResult = (matchingFormId: number) => {
+  return useQuery<IMatchingResult, AxiosError>({
+    queryKey: ["useGetMatchingResult"],
+    queryFn: () =>
+      api.get(`${matchingApiRoute.matchingForm__match}/${matchingFormId}`),
+  });
+};
+
+export interface IMatchingFormResponse extends IMatchingForm {
+  matchingFormId: number;
+}
+
+export const useGetMatchingForm = () => {
+  return useQuery<IMatchingFormResponse, AxiosError>({
+    queryKey: ["useGetMatchingForm"],
+    queryFn: () => api.get(matchingApiRoute.matchingForm),
+    enabled: false,
+  });
+};
+
+export const usePutMatchingForm = () => {
+  return useMutation<AxiosResponse, AxiosError, IUpdateMatchingForm>({
+    mutationFn: (data) => api.put(matchingApiRoute.matchingForm, data),
+    retry: false,
+  });
+};
